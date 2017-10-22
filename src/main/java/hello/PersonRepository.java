ab5b8095e641e94663d8e2aeec9242eeb360b8f1
Mark Vallis <1034528@my.brunel.ac.uk>
2017-10-22 20:03:33 +0100

    simplified repository connection code

--- src/main/java/hello/PersonRepository.java
+++ src/main/java/hello/PersonRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // automatically creates an endpoint at /people for you to hit
-@RepositoryRestResource(path = "people")
+@RepositoryRestResource
 public interface PersonRepository extends MongoRepository<Person, String> {
 }
