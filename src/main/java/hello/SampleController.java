19cba9bf1f4036021421d83cda8b60987baba446
Mark Vallis <1034528@my.brunel.ac.uk>
2017-11-11 15:14:37 +0000

    small refactor for readability

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
