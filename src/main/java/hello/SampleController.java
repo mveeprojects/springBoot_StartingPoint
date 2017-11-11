07cc667b6fdf7cfc50e4680d5440e971527d184d
Mark Vallis <1034528@my.brunel.ac.uk>
2017-11-11 15:08:45 +0000

    Thymeleaf spring boot example showing use of direct variable access and iteration within web template working as expected

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -1,13 +1,28 @@
 package hello;
 
-import org.springframework.web.bind.annotation.*;
-import java.io.IOException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class SampleController {
 
     @RequestMapping("/")
-    public String home() throws IOException {
-        return "Hello World!";
+    public String homeController(Model model) {
+        model.addAttribute("title", "Home");
+        model.addAttribute("content", "Hello there :)");
+        model.addAttribute("listOfStrings", getListOfStrings());
+        return "index";
+    }
+
+    private List<String> getListOfStrings(){
+        List<String> sampleList = new ArrayList<String>();
+        sampleList.add("Bacon");
+        sampleList.add("Eggs");
+        sampleList.add("Gofer");
+        return sampleList;
     }
 }
